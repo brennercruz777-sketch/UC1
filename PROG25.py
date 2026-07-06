6bb74cf4df8d89da301ca84977417323570e1dd9
brennercruz777-sketch <brenner.cruz777@gmail.com>
2026-07-06 20:45:16 -0300

    prog25.py alt

---
+++ PROG25.py
@@ -0,0 +1,9 @@
+v1=int(input('Digite um valor '))
+v2=int(input('Digite um segundo valor '))
+if v1 > v2:
+    print(f'O valor {v1} e maior que o valor {v2}')
+elif v2 > v1:
+    print(f'O valor {v2} e maior que o valor {v1}')
+else:
+    print('Os valores são iguais')
+    
